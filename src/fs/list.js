8adf03e9e0f4ed10ae7bf88d83ec1ae5fde238b3
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-03 13:21:02 +0200

    feat: solve list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import fs from 'node:fs';
+
+const PATH = 'files';
+
 const list = async () => {
-    // Write your code here 
+    
+    fs.stat(PATH, (err) => {
+
+        if(err) {
+            throw new Error('FS operation failed')
+        }
+
+        fs.readdir(PATH, (err, files) => {
+            
+            if(err) throw err;
+
+            console.log('Files in the folder:')
+            files.forEach((file) => {
+                console.log(file)
+            })
+        })
+    })
 };
 
 await list();
