05370f5b634ebf74b1fe051a6fd8f9d3bde48f6d
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-03 12:15:06 +0200

    feat: solve create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+
+const PATH = './files/fresh.txt';
+
 const create = async () => {
     // Write your code here 
+    fs.stat(PATH, (err) => {
+        
+        if(err) {
+            fs.writeFile(PATH, 'I am fresh and young', () => {})
+        } else {
+            throw new Error('FS operation failed')
+        }
+    })
 };
 
 await create();
