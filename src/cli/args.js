f6b324529ea7c9fcd2f8c0b7b152be1e83bfef9b
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-03 14:14:19 +0200

    feat: solve args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,23 @@
 const parseArgs = () => {
-    // Write your code here 
+
+    const args = process.argv
+    .slice(2)
+    .reduce(((acc, item, index, arr) => {
+
+        if(item.startsWith('--')) {
+            item = `${item.slice(2)} is `
+        } else {
+
+            if(index !== arr.length - 1) {
+                item = `${item}, `
+            }
+        }
+
+        return acc += `${item}`
+
+    }), '')
+
+    console.log(args)
 };
 
 parseArgs();
