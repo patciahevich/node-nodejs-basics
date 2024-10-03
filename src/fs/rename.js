bfe73da335d018958f315eea226bc90cc529f1b5
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-03 13:09:50 +0200

    feat: solve rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import fs from 'node:fs';
+import path from 'path';
+
+const oldName = 'wrongFilename.txt';
+const newName = 'properFilename.md';
+const PATH = 'files';
+
 const rename = async () => {
-    // Write your code here 
+    
+    fs.stat(path.join(PATH, oldName), (err) => {
+
+        if(err) {
+            throw new Error('FS operation failed')
+        }
+
+        fs.stat(path.join(PATH, newName), (err) => {
+
+            if(!err) {
+                throw new Error('FS operation failed')
+            }
+
+            fs.rename(path.join(PATH, oldName), path.join(PATH, newName), (err) => {} )
+        })
+
+      
+    })
 };
 
 await rename();
