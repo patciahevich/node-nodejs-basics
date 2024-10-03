851f450e4d29f47f5c1fbe85d3a8977144a113f7
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-03 13:25:43 +0200

    feat: solve read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs';
+import path from 'path';
+
+const PATH = path.join('files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    fs.stat(PATH, (err) => {
+
+        if(err) {
+            throw new Error('FS operation failed')
+        }
+
+        fs.readFile(PATH,  'UTF-8', (err, data) => {
+
+            if(err) throw err;
+
+            console.log(data)
+        })
+    })
 };
 
 await read();
