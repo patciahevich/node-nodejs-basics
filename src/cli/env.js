c9da4e3cd3f8f4cc721811258c1d2cabaeae8004
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-03 13:56:45 +0200

    feat: solve env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+
+const key = 'RSS_'
+
 const parseEnv = () => {
-    // Write your code here 
-};
+
+    const result = Object.entries(process.env).filter((item) => {
+        return item[0].startsWith(key)
+    });
+
+    console.log(`${result[0].join('=')}; ${result[1].join('=')}`)
+
+}
 
 parseEnv();
