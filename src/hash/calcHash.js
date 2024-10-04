08bf7bcf971422979a966d0af6cdf34d697c3121
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-04 10:59:37 +0200

    feat: solve hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createHash } from 'crypto';
+import path from 'path';
+import fs from 'node:fs';
+
+const PATH = path.join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt')
+
 const calculateHash = async () => {
-    // Write your code here 
+
+    const readStream = fs.createReadStream(PATH);
+    readStream.setEncoding('utf-8');
+
+    const hash = createHash('sha256');
+
+    let dataToHash = ''
+
+    readStream
+    .on('data', (chunk) => {
+        dataToHash += chunk;
+
+    })
+    .on('end', () => {
+        console.log(`Hash : ${hash.update(dataToHash).digest('hex')}`)
+    })
+
 };
 
 await calculateHash();
