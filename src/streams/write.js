b614fcecbfb096a36c35f7839602e6f9d15bff4a
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-04 11:33:57 +0200

    feat: solve the write stream task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs';
+import path from 'path';
+
+const PATH = path.join(import.meta.dirname, 'files', 'fileToWrite.txt');
+
+
 const write = async () => {
-    // Write your code here 
+
+    const writeStream = fs.createWriteStream(PATH)
+    console.log('Hello! Write some text! \n(to exit press Ctrl + C) \n')
+
+    process.stdin.on('data', (data) => {
+        writeStream.write(`${data} \n`)
+    })
 };
 
 await write();
