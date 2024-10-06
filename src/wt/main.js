725b93040d3b5dbb1542f19a36ecf8de2cc72d44
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-06 13:30:41 +0200

    feat: solve worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,14 @@
+import { Worker, workerData } from 'node:worker_threads';
+import path from 'path';
+
+const PATH = path.join(import.meta.dirname, 'main.js');
+
 const performCalculations = async () => {
-    // Write your code here
+    
+    const worker = new Worker(PATH, {
+        workerData: 2
+    })
+
 };
 
 await performCalculations();
