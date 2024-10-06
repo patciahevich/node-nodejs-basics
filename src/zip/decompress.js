c419e99a11bde5e514acac6b73bb5fb7d812041c
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-06 11:57:35 +0200

    feat: solve decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs';
+import path from 'path';
+import { createUnzip } from 'node:zlib';
+
+const SRC = path.join(import.meta.dirname, 'files', 'archive.gz');
+const DEST = path.join(import.meta.dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    
+    const source = fs.createReadStream(SRC);
+    const result = fs.createWriteStream(DEST);
+
+    const unzip = createUnzip();
+
+    source
+    .pipe(unzip)
+    .pipe(result)
+    .on('error', (err) => {throw err})
 };
 
 await decompress();
