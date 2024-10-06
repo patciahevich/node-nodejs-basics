ff1bab066735cc7baf045d317f5943b8b8a41554
Volha Patsiahevich <volhapatsiahevich@Volhas-MacBook-Pro.local>
2024-10-06 14:04:12 +0200

    feat: solve worker threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,12 +1,63 @@
 import { Worker, workerData } from 'node:worker_threads';
 import path from 'path';
+import os from 'node:os';
 
-const PATH = path.join(import.meta.dirname, 'main.js');
+const PATH = path.join(import.meta.dirname, 'worker.js');
 
 const performCalculations = async () => {
-    
-    const worker = new Worker(PATH, {
-        workerData: 2
+
+    const coresNumber = os.cpus().length
+    const initialNumber = 10;
+
+    function createWorker(number) {
+        return new Promise((resolve, reject) => {
+
+            const worker = new Worker(PATH, {
+                workerData: number,
+            })
+
+            worker.on('message', (message) => {
+                resolve(message)
+            });
+
+            worker.on('error', (message) => {
+                reject(message)
+            });
+        })
+    }
+
+    function createAllWorkers() {
+        const promises = [];
+
+        for (let i = 0; i < coresNumber; i++) {
+            promises.push(createWorker(initialNumber + i));
+        }
+
+        return promises;
+    }
+
+    Promise.allSettled(createAllWorkers())
+    .then((results) => {
+        return results.map((item) => {
+
+            if(item.status === 'fulfilled') {
+                return {
+                    status: 'resolved',
+                    data: item.value
+                }
+            } else {
+                return {
+                    status: 'error',
+                    data: null
+                }
+            }
+        })
+    })
+    .then((result) => {
+        console.log(result)
+    })
+    .catch((err) => {
+        throw err;
     })
 
 };
